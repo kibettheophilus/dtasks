51a2b869059f56cc50b9d8b6c81b329ef1fbc293
theophiluskibet <kibettheophilus@gmail.com>
2025-10-17 21:35:15 +0300

    setup ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,21 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ktlint)
+}
+
+allprojects{
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        reporters {
+            reporter(ReporterType.JSON)
+        }
+        filter {
+            exclude("**/generated/**")
+            exclude("**/build/**")
+        }
+    }
 }
