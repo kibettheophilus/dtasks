b5a3c44177fcef101f66294d7c94efd478e9282a
theophiluskibet <kibettheophilus@gmail.com>
2025-10-20 17:16:57 +0300

    add missing auth repo declaration

--- app/src/main/java/com/theophiluskibet/dnotes/di/RepositoryModule.kt
+++ app/src/main/java/com/theophiluskibet/dnotes/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.theophiluskibet.dnotes.di
 
+import com.theophiluskibet.dnotes.data.repository.AuthRepositoryImpl
 import com.theophiluskibet.dnotes.data.repository.SyncRepositoryImpl
 import com.theophiluskibet.dnotes.data.repository.TasksRepositoryImpl
+import com.theophiluskibet.dnotes.domain.repository.AuthRepository
 import com.theophiluskibet.dnotes.domain.repository.SyncRepository
 import com.theophiluskibet.dnotes.domain.repository.TasksRepository
 import org.koin.core.module.dsl.singleOf
@@ -10,5 +12,6 @@ import org.koin.dsl.module
 
 val repositoryModule = module {
     singleOf(::TasksRepositoryImpl) bind TasksRepository::class
+    singleOf(::AuthRepositoryImpl) bind AuthRepository::class
     singleOf(::SyncRepositoryImpl) bind SyncRepository::class
 }
