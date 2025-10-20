1c2584c6b86e3f95c0e8f404fa98499e72a08b71
theophiluskibet <kibettheophilus@gmail.com>
2025-10-20 17:32:58 +0300

    change room version to start from 1

--- app/src/main/java/com/theophiluskibet/dnotes/data/local/database/TasksDatabase.kt
+++ app/src/main/java/com/theophiluskibet/dnotes/data/local/database/TasksDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.theophiluskibet.dnotes.data.local.dao.TasksDao
 import com.theophiluskibet.dnotes.data.local.entity.TaskEntity
 
-@Database(version = 0, entities = [TaskEntity::class], exportSchema = false)
+@Database(version = 1, entities = [TaskEntity::class], exportSchema = false)
 abstract class TasksDatabase : RoomDatabase() {
     abstract fun tasksDao(): TasksDao
 }
