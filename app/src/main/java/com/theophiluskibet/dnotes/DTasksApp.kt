3313060dbeaa5b8c31c1a976a9de5ac991c0e55f
theophiluskibet <kibettheophilus@gmail.com>
2025-10-20 17:17:30 +0300

    setup SyncModule.kt

--- app/src/main/java/com/theophiluskibet/dnotes/DTasksApp.kt
+++ app/src/main/java/com/theophiluskibet/dnotes/DTasksApp.kt
@@ -4,13 +4,14 @@ import android.app.Application
 import com.theophiluskibet.dnotes.di.localModule
 import com.theophiluskibet.dnotes.di.networkModule
 import com.theophiluskibet.dnotes.di.repositoryModule
+import com.theophiluskibet.dnotes.di.syncModule
 import org.koin.core.context.startKoin
 
 class DTasksApp : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            modules(listOf(localModule, networkModule, repositoryModule))
+            modules(listOf(localModule, networkModule, repositoryModule, syncModule))
         }
     }
 }
