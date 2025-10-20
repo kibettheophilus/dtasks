dc50f07f414e527def31a7dd159edc5454c9689d
theophiluskibet <kibettheophilus@gmail.com>
2025-10-20 13:07:27 +0300

    update endpoints in TasksApi.kt

--- app/src/main/java/com/theophiluskibet/dnotes/data/remote/api/TasksApi.kt
+++ app/src/main/java/com/theophiluskibet/dnotes/data/remote/api/TasksApi.kt
@@ -3,21 +3,14 @@ package com.theophiluskibet.dnotes.data.remote.api
 import com.theophiluskibet.dnotes.data.remote.models.TaskDto
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface TasksApi {
     @GET("/tasks")
-    suspend fun getTasks(): Response<List<TaskDto>>
+    suspend fun getTasks(@Query("since") since: Long): Response<List<TaskDto>>
 
-    @POST("/tasks")
-    suspend fun createTasks(@Body tasks: List<TaskDto>): Response<List<TaskDto>>
-
-    @PUT("/tasks/{id}")
-    suspend fun updateTask(@Body task: TaskDto): Response<TaskDto>
-
-    @DELETE("/tasks/{id}")
-    suspend fun deleteTask(taskId: String): Response<Any>
+    @POST("/sync")
+    suspend fun syncTasks(@Body tasks: List<TaskDto>): Response<List<TaskDto>>
 }
