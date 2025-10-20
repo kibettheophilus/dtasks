ee1881d904864c106c84ffddf6fafba81370be0a
theophiluskibet <kibettheophilus@gmail.com>
2025-10-20 17:16:41 +0300

    update push tasks fun and add updatelastsynctime

--- app/src/main/java/com/theophiluskibet/dnotes/domain/repository/SyncRepository.kt
+++ app/src/main/java/com/theophiluskibet/dnotes/domain/repository/SyncRepository.kt
@@ -1,9 +1,13 @@
 package com.theophiluskibet.dnotes.domain.repository
 
+import com.theophiluskibet.dnotes.data.local.entity.TaskEntity
 import com.theophiluskibet.dnotes.domain.models.TaskModel
 
 interface SyncRepository {
     suspend fun fetchTasks(): Result<List<TaskModel>>
-    suspend fun pushTasks(localTasks: List<TaskModel>): Result<Boolean>
+    suspend fun pushTasks(): Result<Boolean>
     suspend fun mergeTasks(remoteTasks: List<TaskModel>)
+    suspend fun updateLastSyncTime()
+    suspend fun updateSyncedTasks(taskIds: List<String>)
+    suspend fun getUnsyncedTasks(): List<TaskEntity>
 }
