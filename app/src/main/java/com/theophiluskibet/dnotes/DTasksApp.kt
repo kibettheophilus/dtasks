afc4878e0c604d9783f4bb1eac03d8978d67b999
theophiluskibet <kibettheophilus@gmail.com>
2025-10-20 17:20:14 +0300

    update DTasksApp.kt

--- app/src/main/java/com/theophiluskibet/dnotes/DTasksApp.kt
+++ app/src/main/java/com/theophiluskibet/dnotes/DTasksApp.kt
@@ -5,13 +5,22 @@ import com.theophiluskibet.dnotes.di.localModule
 import com.theophiluskibet.dnotes.di.networkModule
 import com.theophiluskibet.dnotes.di.repositoryModule
 import com.theophiluskibet.dnotes.di.syncModule
+import com.theophiluskibet.dnotes.sync.schedulePeriodicSync
+import com.theophiluskibet.dnotes.sync.triggerImmediateSync
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.workmanager.koin.workManagerFactory
 import org.koin.core.context.startKoin
 
 class DTasksApp : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
+            androidContext(this@DTasksApp)
             modules(listOf(localModule, networkModule, repositoryModule, syncModule))
+            workManagerFactory()
+
+            schedulePeriodicSync()
+            triggerImmediateSync()
         }
     }
 }
