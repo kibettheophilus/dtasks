565daf1d8192b14fd71d73b32f127291977f7e4e
theophiluskibet <kibettheophilus@gmail.com>
2025-10-20 13:29:15 +0300

    delete AuthResponse.kt

--- app/src/main/java/com/theophiluskibet/dnotes/data/remote/models/AuthResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.theophiluskibet.dnotes.data.remote.models
-
-data class AuthResponse(
-    val token: String,
-    val tokenExpiry: String,
-    val refreshToken: String,
-    val refreshTokenExpiry: String
-)
