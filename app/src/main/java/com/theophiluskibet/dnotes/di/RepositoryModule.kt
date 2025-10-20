d0e176ae4af4065c3da13354e53044ee992a0c2f
theophiluskibet <kibettheophilus@gmail.com>
2025-10-20 16:36:33 +0300

    update RepositoryModule.kt

--- app/src/main/java/com/theophiluskibet/dnotes/di/RepositoryModule.kt
+++ app/src/main/java/com/theophiluskibet/dnotes/di/RepositoryModule.kt
@@ -1,6 +1,8 @@
 package com.theophiluskibet.dnotes.di
 
+import com.theophiluskibet.dnotes.data.repository.SyncRepositoryImpl
 import com.theophiluskibet.dnotes.data.repository.TasksRepositoryImpl
+import com.theophiluskibet.dnotes.domain.repository.SyncRepository
 import com.theophiluskibet.dnotes.domain.repository.TasksRepository
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
@@ -8,4 +10,5 @@ import org.koin.dsl.module
 
 val repositoryModule = module {
     singleOf(::TasksRepositoryImpl) bind TasksRepository::class
+    singleOf(::SyncRepositoryImpl) bind SyncRepository::class
 }
