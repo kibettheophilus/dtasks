5322b1fbdc98f0c0a2f855155ab9a84e88546adc
theophiluskibet <kibettheophilus@gmail.com>
2025-10-21 16:14:49 +0300

    add material icons deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.material.icons)
 
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
