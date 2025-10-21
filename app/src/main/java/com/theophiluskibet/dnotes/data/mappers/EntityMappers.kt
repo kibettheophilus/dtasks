792b7dde7e6552a8f90520263d9e6c8db999549e
theophiluskibet <kibettheophilus@gmail.com>
2025-10-21 16:16:50 +0300

    update EntityMappers.kt

--- app/src/main/java/com/theophiluskibet/dnotes/data/mappers/EntityMappers.kt
+++ app/src/main/java/com/theophiluskibet/dnotes/data/mappers/EntityMappers.kt
@@ -3,6 +3,8 @@ package com.theophiluskibet.dnotes.data.mappers
 import com.theophiluskibet.dnotes.data.local.entity.TaskEntity
 import com.theophiluskibet.dnotes.data.remote.models.TaskDto
 import com.theophiluskibet.dnotes.domain.models.TaskModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun TaskEntity.toDomain() = TaskModel(
     id = id,
@@ -18,7 +20,9 @@ fun TaskEntity.toDto() = TaskDto(
     id = id,
     title = title,
     description = description,
-    dueDate = dueDate,
+    dueDate = dueDate?.let {
+        SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault()).format(it)
+    } ?: "",
     isCompleted = isCompleted,
     createdAt = createdAt,
     updatedAt = updatedAt
