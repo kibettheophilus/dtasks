e672aa70d7175c968cb1d4e0af154efcf3dd0571
theophiluskibet <kibettheophilus@gmail.com>
2025-10-21 16:17:01 +0300

    update DtoMappers.kt

--- app/src/main/java/com/theophiluskibet/dnotes/data/mappers/DtoMappers.kt
+++ app/src/main/java/com/theophiluskibet/dnotes/data/mappers/DtoMappers.kt
@@ -2,12 +2,20 @@ package com.theophiluskibet.dnotes.data.mappers
 
 import com.theophiluskibet.dnotes.data.remote.models.TaskDto
 import com.theophiluskibet.dnotes.domain.models.TaskModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun TaskDto.toDomain() = TaskModel(
     id = id,
     title = title,
     description = description,
-    dueDate = dueDate,
+    dueDate = try {
+        if (dueDate.isNotEmpty()) {
+            SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault()).parse(dueDate)
+        } else null
+    } catch (e: Exception) {
+        null
+    },
     isCompleted = isCompleted,
     createdAt = createdAt,
     updatedAt = updatedAt
