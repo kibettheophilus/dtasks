89c1187015cb16fdc052f111a2299f7d00dd70cd
theophiluskibet <kibettheophilus@gmail.com>
2025-10-21 16:16:07 +0300

    change duedate type in TaskModel.kt

--- app/src/main/java/com/theophiluskibet/dnotes/domain/models/TaskModel.kt
+++ app/src/main/java/com/theophiluskibet/dnotes/domain/models/TaskModel.kt
@@ -1,11 +1,13 @@
 package com.theophiluskibet.dnotes.domain.models
 
+import java.util.Date
+
 data class TaskModel(
     val id: String,
     val title: String,
-    val description: String,
-    val dueDate: String,
-    val isCompleted: Boolean,
+    val description: String = "",
+    val dueDate: Date?,
+    val isCompleted: Boolean = false,
     val createdAt: String,
     val updatedAt: String
 )
