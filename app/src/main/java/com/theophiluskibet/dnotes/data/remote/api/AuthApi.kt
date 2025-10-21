449c1f4bc6f501564ac0ba0bafbae9ced250896a
theophiluskibet <kibettheophilus@gmail.com>
2025-10-21 15:39:08 +0300

    fix fissing @Body annotation in AuthApi.kt

--- app/src/main/java/com/theophiluskibet/dnotes/data/remote/api/AuthApi.kt
+++ app/src/main/java/com/theophiluskibet/dnotes/data/remote/api/AuthApi.kt
@@ -1,9 +1,10 @@
 package com.theophiluskibet.dnotes.data.remote.api
 
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthApi {
     @POST("/auth/login")
-    suspend fun login(email: String): Response<String>
+    suspend fun login(@Body email: String): Response<String>
 }
