8dea48611a54ea8230320ec0641955ebaf390ae6
theophiluskibet <kibettheophilus@gmail.com>
2025-10-23 00:04:10 +0300

    add @SerialName to field in TaskDto.kt

--- app/src/main/java/com/theophiluskibet/dtasks/data/remote/models/TaskDto.kt
+++ app/src/main/java/com/theophiluskibet/dtasks/data/remote/models/TaskDto.kt
@@ -1,5 +1,6 @@
 package com.theophiluskibet.dtasks.data.remote.models
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -7,7 +8,8 @@ data class TaskDto(
     val id: String,
     val title: String,
     val description: String,
-    val dueDate: String,
+    val dueDate: String?,
+    @SerialName("completed")
     val isCompleted: Boolean,
     val createdAt: String,
     val updatedAt: String
